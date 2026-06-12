4f0ce71e0bae64f9e1db94fae89cda511a91c52f
adambluet518 <adamblue.t518@gmail.com>
2026-06-12 16:35:12 -0600

    Delete root directory

--- root/js/characters/js:characters:guard.js
+++
@@ -1,9 +0,0 @@
-// js/characters/guard.js
-import { BaseCharacter } from './baseCharacter.js';
-export class Guard extends BaseCharacter {
-  constructor(name, race, position, faction) {
-    super(name, race, 'Guard', position);
-    this.faction = faction;
-    this.aggroRange = 15;
-  }
-}
